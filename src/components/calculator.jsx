bbc8478d60a47b455ff9a793c41fcb1c329b6e44
fasihnasir <154458171+fasih-nasir@users.noreply.github.com>
2024-08-27 15:14:58 +0500

    Update calculator.jsx

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,63 +1,86 @@
 import { useState } from 'react'
 function Calculator(){
     let [num,setNum] = useState(" ")
+    let [ans,setAns] = useState(" ")
+    
+    function playSound() {
+      const audio = new Audio('../assets/audio.mp3'); 
+       audio.play();
+
+       
+    }
     function fn(num){
    
-     
-   setNum(prev => prev + num)}
+      playSound()
+   setNum(prev => prev + num)
+
+
+  
+  
+  }
    function eq(){
      let ans= eval(num);
      
-     setNum(ans)
-     console.log(ans);
+
+
+     setNum(num)
+     setAns(ans)
+
+    //  console.log(ans);
    
    
      
    }
    function clearOnce(){
      let clearonce=num.slice(0,-1);
+    
+ 
+     
      setNum(clearonce)
+
    }
    function clearAll(){
      // let clearonce=num.slice(0,-1);
      setNum("")
+     setAns("")
    }
     return(
     <>
-       <div className='flex flex-col   h-[100vh]'>
-      <div className='m-auto'>
-<input type="text" className='p-5' value={num} readOnly/>
+       <div className='flex flex-col   h-[90vh] m-auto w-[50vh] div1'>
+      <div className='divinp'>
+<input type="text" className='' value={num} readOnly/>
+<input type="text" className='inp2'  value={ans} readOnly/>
 </div>
 
 <div className='flex flex-col m-auto'>
  <div>
-<button className='border bg-white text-black p-2 col-span-2' onClick={clearAll}>AC</button>
-<button className='border bg-white text-black p-2' onClick={clearOnce}>CE</button>
-<button className='border bg-white text-black p-2' onClick={() => fn('+')}>+</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('%')}>%</button>
+<button className='border bg-white text-black  col-span-2' onClick={clearAll}>AC</button>
+<button className='border bg-white text-black ' onClick={clearOnce}>CE</button>
+<button className='border bg-white text-black ' onClick={() => fn('+')}>+</button>
+          <button className='border bg-white text-black ' onClick={() => fn('%')}>%</button>
           </div>
   <div>
-          <button className='border bg-white text-black p-2' onClick={() => fn('7')}>7</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('8')}>8</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('9')}>9</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('/')}>/</button>
+          <button className='border bg-white text-black ' onClick={() => fn('7')}>7</button>
+          <button className='border bg-white text-black ' onClick={() => fn('8')}>8</button>
+          <button className='border bg-white text-black ' onClick={() => fn('9')}>9</button>
+          <button className='border bg-white text-black ' onClick={() => fn('/')}>/</button>
           </div>
           <div>   
-          <button className='border bg-white text-black p-2' onClick={() => fn('4')}>4</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('5')}>5</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('6')}>6</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('*')}>X</button>
+          <button className='border bg-white text-black ' onClick={() => fn('4')}>4</button>
+          <button className='border bg-white text-black ' onClick={() => fn('5')}>5</button>
+          <button className='border bg-white text-black ' onClick={() => fn('6')}>6</button>
+          <button className='border bg-white text-black ' onClick={() => fn('*')}>X</button>
           </div>
           <div>
-          <button className='border bg-white text-black p-2' onClick={() => fn('1')}>1</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('2')}>2</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('3')}>3</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('-')}>-</button>
+          <button className='border bg-white text-black ' onClick={() => fn('1')}>1</button>
+          <button className='border bg-white text-black ' onClick={() => fn('2')}>2</button>
+          <button className='border bg-white text-black ' onClick={() => fn('3')}>3</button>
+          <button className='border bg-white text-black ' onClick={() => fn('-')}>-</button>
           </div>
           <div>
-          <button className='border bg-white text-black p-2' onClick={() => fn('0')}>0</button>
-          <button className='border bg-white text-black p-2' onClick={() => fn('.')}>.</button>
-          <button className='border bg-white text-black p-2 w-[130px]' onClick={eq}>=</button>
+          <button className='border bg-white text-black' onClick={() => fn('0')}>0</button>
+          <button className='border bg-white text-black' onClick={() => fn('.')}>.</button>
+          <button className='border bg-white text-black w-[130px]' onClick={eq}>=</button>
 
           </div>
  
